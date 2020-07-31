c073423fdafd280270911e28d3f2483c05ac4d86
eeshashetty <ekshetty2410@gmail.com>
2020-07-31 23:23:22 +0530

    Updates

---
+++ precache-manifest.c5771f48f84acb6b6f2e7c7db3228ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fab31e1b84ebc556f7f116cde8a7d5a",
+    "url": "/tic-tac-bot/index.html"
+  },
+  {
+    "revision": "9eaeeaf6f54d38276956",
+    "url": "/tic-tac-bot/static/css/main.1be110c8.chunk.css"
+  },
+  {
+    "revision": "a40b2d4f49dde0be56a3",
+    "url": "/tic-tac-bot/static/js/2.79624704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-bot/static/js/2.79624704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eaeeaf6f54d38276956",
+    "url": "/tic-tac-bot/static/js/main.6a6a432e.chunk.js"
+  },
+  {
+    "revision": "afd2f031addf14c234f6",
+    "url": "/tic-tac-bot/static/js/runtime-main.e5c73100.js"
+  }
+]);
